76085a1b1412b07545824058b93633bb85c2c8ba
gbiagi <geanfrancobiagi@hotmail.com>
2023-10-08 22:19:10 +0200

    Intento mobile

--- Project/src/ControllerMobile1.java
+++ Project/src/ControllerMobile1.java
@@ -2,17 +2,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ControllerMobile1 {
     @FXML
-    ListView<String> listaObjetos = new ListView<>();
-    ObservableList<String> items = FXCollections.observableArrayList("a");
+    private VBox yPane;
     public void loadListMobile(String opcio) {
         // Obtenir una referència a AppData que gestiona les dades
         AppData appData = AppData.getInstance();
@@ -24,32 +25,40 @@ public class ControllerMobile1 {
                 try {
                     showListMobile(opcio);
                 } catch (Exception e) {
-                    e.printStackTrace();
                     System.out.println("ControllerMobile: Error showing list.");
                 }
             }
         });
     }
     public void showListMobile(String opcioSeleccionada) throws Exception {
+        // Carregar la plantilla
+        URL resource = this.getClass().getResource("assets/template_list_item.fxml");
         // Obtenir una referència a l'ojecte AppData que gestiona les dades
         AppData appData = AppData.getInstance();
-        System.out.println("E2");
         // Obtenir les dades de l'opció seleccionada
         JSONArray dades = appData.getData(opcioSeleccionada);
-        System.out.println("E4");
+        // Esborrar la llista actual
+        yPane.getChildren().clear();
         // Carregar la llista amb les dades
-        int j = 5;
         for (int i = 0; i < dades.length(); i++) {
-            j++;
             JSONObject consoleObject = dades.getJSONObject(i);
             if (consoleObject.has("nom")) {
                 String nom = consoleObject.getString("nom");
-                System.out.println(nom);
-                items.addAll(nom);
+                String imatge = "assets/images/" + consoleObject.getString("imatge");
+                FXMLLoader loader = new FXMLLoader(resource);
+                Parent itemTemplate = loader.load();
+                ControllerListItem itemController = loader.getController();
+                itemController.setText(nom);
+                itemController.setImage(imatge);
+                // Defineix el callback que s'executarà quan l'usuari seleccioni un element
+                // (cal passar final perquè es pugui accedir des del callback)
+                final String type = opcioSeleccionada;
+                final int index = i;
+//                itemTemplate.setOnMouseClicked(event -> {
+//                    showInfo(type, index);
+//                });
+                yPane.getChildren().add(itemTemplate);
             }
         }
-        listaObjetos.setItems(items);
-        System.out.println("aa****");
-        System.out.println(listaObjetos);
     }
 }
